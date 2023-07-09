6da1c6504592a227e3d3c2031ac255f82dee9c6b
Juan Villasmil <villasmiljuandiego@gmail.com>
2023-07-08 20:43:48 -0400

    botones funcionales para cambiar de pantalla

---
+++ Tesis 2023/Assets/Scripts/CambiarEscena.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CambiarEscena : MonoBehaviour
+{
+    public string nombreEscena = "2";
+
+    private void Start()
+    {
+        Button boton = GetComponent<Button>();
+        boton.onClick.AddListener(CargarEscena);
+    }
+
+    private void CargarEscena()
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+}
