c49356e63d73d906eac581eb73616ed6e2c5b292
Juan Villasmil <villasmiljuandiego@gmail.com>
2023-09-02 00:26:45 -0400

    adelanto juego de sliding puzzle

---
+++ bliss/Assets/Scripts/SlidingScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SlidingScript : MonoBehaviour
+{
+    [SerializeField] private Transform emptySpace = null;
+    private Camera _camera;
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit)
+            {
+                if (Vector2. Distance(emptySpace.position, hit.transform.position) < 2)
+                {
+                    Vector2 lastEmptySpacePosition = emptySpace.position;
+                    emptySpace.position = hit.transform.position;
+                    hit.transform.position = lastEmptySpacePosition;
+                }
+            }
+        }
+    }
+}
