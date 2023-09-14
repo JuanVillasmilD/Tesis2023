62d3c3cdffeaeb0c1071bb2b94dea7a68f4409b0
Juan Villasmil <villasmiljuandiego@gmail.com>
2023-09-14 13:18:24 -0400

    finalizadas animaciones del apartado de respiracion
    Ya se pueden seleccionar cada uno de los minutos y te lleva al canvas con su animación respectiva

--- bliss/Assets/Scripts/RespiracionMinutos.cs
+++ bliss/Assets/Scripts/RespiracionMinutos.cs
@@ -3,40 +3,54 @@ using UnityEngine.UI;
 
 public class RespiracionMinutos : MonoBehaviour
 {
-    public GameObject object1;
-    public GameObject object2;
-    public GameObject object3;
+    public GameObject objeto1;
+    public GameObject objeto2;
+    public GameObject objeto3;
+    public GameObject objeto4;
 
-    public Button button1;
-    public Button button2;
-    public Button button3;
+    public Button boton1;
+    public Button boton2;
+    public Button boton3;
+    public Button boton4;
 
     private void Start()
     {
         // Configura las funciones de los botones para cambiar de objeto
-        button1.onClick.AddListener(ShowObject1);
-        button2.onClick.AddListener(ShowObject2);
-        button3.onClick.AddListener(ShowObject3);
+        boton1.onClick.AddListener(ShowObjeto1);
+        boton2.onClick.AddListener(ShowObjeto2);
+        boton3.onClick.AddListener(ShowObjeto3);
+        boton4.onClick.AddListener(ShowObjeto4);
     }
 
-    private void ShowObject1()
+    private void ShowObjeto1()
     {
-        object1.SetActive(true);
-        object2.SetActive(false);
-        object3.SetActive(false);
+        objeto1.SetActive(true);
+        objeto2.SetActive(false);
+        objeto3.SetActive(false);
+        objeto4.SetActive(false);
     }
 
-    private void ShowObject2()
+    private void ShowObjeto2()
     {
-        object1.SetActive(false);
-        object2.SetActive(true);
-        object3.SetActive(false);
+        objeto1.SetActive(false);
+        objeto2.SetActive(true);
+        objeto3.SetActive(false);
+        objeto4.SetActive(false);
     }
 
-    private void ShowObject3()
+    private void ShowObjeto3()
     {
-        object1.SetActive(false);
-        object2.SetActive(false);
-        object3.SetActive(true);
+        objeto1.SetActive(false);
+        objeto2.SetActive(false);
+        objeto3.SetActive(true);
+        objeto4.SetActive(false);
+    }
+
+    private void ShowObjeto4()
+    {
+        objeto1.SetActive(false);
+        objeto2.SetActive(false);
+        objeto3.SetActive(false);
+        objeto4.SetActive(true);
     }
 }
