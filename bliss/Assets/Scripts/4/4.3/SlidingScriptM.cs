5fb86b1922fcd7acae72e0782fae6cffec5f4367
Juan Villasmil <villasmiljuandiego@gmail.com>
2023-10-25 01:51:06 -0400

    Update: Barra de inicio
    -Aumentado el tamaño del botón para que sea más fácil seleccionar elementos desde el teléfono.
    -Eliminados algunos scripts viejos sin uso.

--- bliss/Assets/Scripts/4/4.3/SlidingScriptM.cs
+++ bliss/Assets/Scripts/4/4.3/SlidingScriptM.cs
@@ -38,7 +38,7 @@ public class SlidingScriptM : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
             if (hit)
             {
-                if (Vector2.Distance(emptySpace.position, hit.transform.position) < 1.7)
+                if (Vector2.Distance(emptySpace.position, hit.transform.position) < 1.5)
                 {
                     Vector2 lastEmptySpacePosition = emptySpace.position;
                     TilesScript thisTile = hit.transform.GetComponent<TilesScript>();
